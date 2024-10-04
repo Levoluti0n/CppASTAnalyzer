a416ca2621c5a04f4b37000563fc2b245894eb74
Levon <levon.mezhlumyan.h@gmail.com>
2024-10-04 10:29:49 +0400

    Update Lexer structure

--- include/Lexer.hpp
+++ include/Lexer.hpp
@@ -1,22 +1,32 @@
 #ifndef LEXER_HPP
 #define LEXER_HPP
 
+#include <unordered_set>
 #include "Token.hpp"
 #include <string>
 #include <vector>
 
 class Lexer {
     public:
-        Lexer(const std::string&);
+        explicit Lexer(const std::string&);
         std::vector<Token> tokenize();
     private:
-        std::string sourceCode;
+        std::string source;
         size_t currentPosition;
+        int currentLine;
 
-        char getChar();
-        Token nextToken();
-        char peekChar() const;
+        Token createIdentifierOrKeyword(const std::string&);
+        Token createNumber(const std::string&);
+        Token createDelimiter(const char);
+        Token createOperator(const char);
         void skipWhitespace();
+        Token nextToken();
+        char getChar();
+
+        const std::unordered_set<std::string> keywords = {
+            "int", "float", "double", "char", "void", "class", "public", "private", "protected",
+            "if", "else", "while", "for", "return", "static", "namespace", "using", "include"
+        };
 };
 
 #endif
